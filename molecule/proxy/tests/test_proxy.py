f6a25c131b12b0a233a601149c66d183afc91643
Delibes Bechir BKWEDOU-NGAMENI <60311355+bngameni@users.noreply.github.com>
2024-03-22 15:02:01 +0100

    Revert "Feat: load callback plugins for print task execution time"

--- molecule/proxy/tests/test_proxy.py
+++ molecule/proxy/tests/test_proxy.py
@@ -1,5 +1,13 @@
 import os
 
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
+# have a look at https://testinfra.readthedocs.io/en/latest/modules.html to see the different possible tests
+
 
 def test_proxy_is_configured(host):
     assert "https://monproxy:3128" in host.check_output("npm config ls -l")
--- molecule/shared/test_default.py
+++ molecule/shared/test_default.py
@@ -1,5 +1,13 @@
 import os
 
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
+# have a look at https://testinfra.readthedocs.io/en/latest/modules.html to see the different possible tests
+
 
 def test_nodejs_is_installed(host):
     nodejs_package = host.package("nodejs")
